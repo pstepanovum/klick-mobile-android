fd54222607cabe080a170cd6590386c628ea6b5e
pstepanovum <pas273@miami.edu>
2026-06-29 14:31:24 -0400

    Sync version to 0.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.klicmobile.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.3.4"
+        versionCode = 26
+        versionName = "0.3.5"
     }
 
     buildTypes {
