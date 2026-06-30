81842ae24f519d1422443a7d3e060e2df973f23a
pstepanovum <pas273@miami.edu>
2026-06-30 14:02:15 -0400

    Release v0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.klicmobile.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.4.0"
+        versionCode = 33
+        versionName = "0.4.1"
     }
 
     buildTypes {
