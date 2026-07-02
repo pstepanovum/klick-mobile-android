0fc2cbf70bee6e7c6365ba0bd103597d749992be
pstepanovum <pas273@miami.edu>
2026-07-01 23:12:55 -0400

    Release v0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.klic.mobile.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "0.4.2"
+        versionCode = 35
+        versionName = "0.4.3"
         buildConfigField("String", "KLIC_API_ORIGIN", stringBuildConfig("KLIC_API_ORIGIN", "https://api.89.34.230.2.sslip.io"))
     }
 
